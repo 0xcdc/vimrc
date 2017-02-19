ceeb8debd5bd84e73e116bbd69b8798f0ecbfd03
Charlie Carson <charliecarson@gmail.com>
2017-02-19 12:12:04 -0600

    git settings

---
+++ setup-git.sh
@@ -0,0 +1,7 @@
+git config --global user.email "charliecarson@gmail.com"
+git config --global user.name "Charlie Carson"
+git config --global core.editor "vi"
+git config --global push.default simple
+
+
+
