f1fa703dfd060c22a5e55cefadd296f75830c70b
Charlie Carson <charliecarson@gmail.com>
2018-12-22 19:21:35 -0800

    editor to vim

--- setup-git.sh
+++ setup-git.sh
@@ -1,6 +1,6 @@
 git config --global user.email "charliecarson@gmail.com"
 git config --global user.name "Charlie Carson"
-git config --global core.editor "vi"
+git config --global core.editor "vim"
 git config --global push.default simple
 
 
